39bf5ec1c1c72ee674f35747a21689016dfe1bf2
Dan Allinder <dcallinder1s@gmail.com>
2017-06-14 10:15:28 -0600

    make a few more changes

--- lesson_2/refractoring_calculator.rb
+++ lesson_2/refractoring_calculator.rb
@@ -3,7 +3,7 @@ def prompt(message)
 end
 
 def valid_number?(num)
-  num.to_i != 0 
+  num.to_i != 0
 end
 
 def operation_to_message(op)
@@ -19,14 +19,13 @@ def operation_to_message(op)
   end
 end
 
-
 prompt 'Welcome to Calculator! Enter your name:'
 
 name = ''
 loop do
   name = gets.chomp
 
-  if name.empty?
+  if name.empty? 
     prompt 'Make sure that is a vaild name.'
   else
     break
@@ -35,14 +34,13 @@ end
 
 prompt "Hi #{name}!"
 
-loop do #main loop
-
+loop do # main loop
   number1 = ''
   loop do
     prompt "What's the first number?"
     number1 = gets.chomp
 
-    if valid_number?(number1)
+    if valid_number?(number1) 
       break
     else
       prompt("Hmm... that doesn't look like a vaild number")
@@ -54,7 +52,7 @@ loop do #main loop
     prompt "What's the second number?"
     number2 = gets.chomp
 
-    if valid_number?(number2)
+    if valid_number?(number2) 
       break
     else
       prompt("Hmm... that doesn't look like a vaild number")
@@ -74,26 +72,26 @@ loop do #main loop
   loop do
     operator = gets.chomp
 
-    if %w(1 2 3 4).include?(operator)
+    if %w(1 2 3 4).include?(operator) 
       break
     else
       prompt 'Must choose 1, 2, 3 or 4'
     end
   end
 
-prompt "#{operation_to_message(operator)} the two numbers..."
+  prompt "#{operation_to_message(operator)} the two numbers..."
 
   result = case operator
-            when '1'
-              number1.to_i + number2.to_i
-            when '2'
-              number1.to_i - number2.to_i
-            when '3'
-              number1.to_i * number2.to_i
-            when '4'
-              number1.to_f / number2.to_f
+           when '1'
+             number1.to_i + number2.to_i
+           when '2'
+             number1.to_i - number2.to_i
+           when '3'
+             number1.to_i * number2.to_i
+           when '4'
+             number1.to_f / number2.to_f
   
-  end
+           end
 
   prompt "The result is #{result}"
 
