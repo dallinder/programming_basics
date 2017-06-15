10326f6259020a05f2398a5abbfaed4f81946f90
Dan Allinder <dcallinder1s@gmail.com>
2017-06-15 11:48:58 -0600

    refactored game to get rid of rubocop errors

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,19 +1,20 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = %w(rock paper scissors).freeze
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-def display_result(player, computer)
-   if (player == 'rock' && computer == 'scissors') ||
-      (player == 'paper' && computer == 'rock') ||
-      (player == 'scissors' && computer == 'paper')
-    prompt("You won!")
-  elsif (player == 'rock' && computer == 'paper') ||
-        (player == 'paper' && computer =='scissors') ||
-        (player == 'scissors' && computer == 'rock')
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
 
-    prompt("Computer won!")
+def display_result(player, computer)
+  if win?(player, computer)
+    prompt('You won!')
+  elsif win?(computer, player)
+    prompt('Computer won!')
   else
     prompt("It's a tie!")
   end
@@ -28,7 +29,7 @@ loop do
     if VALID_CHOICES.include?(choice)
       break
     else
-      prompt("That's not a vaild choice.")
+      prompt('That\'s not a vaild choice.')
     end
   end
 
@@ -38,10 +39,9 @@ loop do
 
   display_result(choice, computer_choice)
 
-  prompt("Do you want to play again?")
+  prompt('Do you want to play again?')
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt("Thank you for playing. Bye!")
-
+prompt('Thank you for playing. Bye!')
