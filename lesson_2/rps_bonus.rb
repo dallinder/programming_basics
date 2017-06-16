a23d27da262a3e4d0880542938772065d2fc2f87
Dan Allinder <dcallinder1s@gmail.com>
2017-06-15 19:53:13 -0600

    made changes to make the code simpler and resolve some rubocop issues with too many lines

--- lesson_2/rps_bonus.rb
+++ lesson_2/rps_bonus.rb
@@ -25,20 +25,21 @@ end
 player_score = 0
 computer_score = 0
 
-prompt('Welcome to Rock, Paper, Scissors, Lizard Spock!')
+prompt('Welcome to Rock, Paper, Scissors, Lizard, Spock!')
 prompt('You will play against the computer.')
 prompt('First one to 5 wins, takes the game! ')
 
 loop do
   choice = ''
   loop do
-    prompt("Choose one: r = rock, p = paper, s = scissors, l = lizard, sp = spock")
+    prompt('Choose one: r = rock, p = paper, s = scissors, ' \
+      'l = lizard, sp = spock')
     choice = gets.chomp
 
     if VALID_CHOICES.include?(choice)
       break
     else
-      prompt('That\'s not a vaild choice.')
+      prompt("That's not a vaild choice.")
     end
   end
 
@@ -53,18 +54,23 @@ loop do
   elsif win?(computer_choice, choice)
     computer_score += 1
   end
-      
-  prompt("Your score is: #{player_score}. The computer score is: #{computer_score}")
-  puts #blank line 
+
+  prompt("Your score is: #{player_score}." \
+    "The computer score is: #{computer_score}")
+  puts # blank line
 
   if player_score >= 5
     prompt('YOU WON!')
-    break
+    prompt('Do you want to play again?')
+    answer = gets.chomp
+    break unless answer.downcase.start_with?('y')
   elsif computer_score >= 5
     prompt('YOU LOST!')
-    break
+    prompt('Do you want to play again?')
+    answer = gets.chomp
+    break unless answer.downcase.start_with?('y')
   end
 end
 
-puts #blank line
+puts # blank line
 prompt('Thank you for playing. Bye!')
