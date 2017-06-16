bceb68f35c223a6fcf9167a4ddb79e9c4fcbc480
Dan Allinder <dcallinder1s@gmail.com>
2017-06-16 12:11:30 -0600

    Finished Easy 1

---
+++ lesson_3/easy_1_6.rb
@@ -0,0 +1,9 @@
+# 1st way to do it
+famous_words = 'seven years ago...'
+
+famous_words.insert(0, 'Four score and ')
+
+# 2nd way to do it
+famous_words = 'seven years ago...'
+
+famous_words.prepend('Four score and ')
